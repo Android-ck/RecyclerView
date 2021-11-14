922289f1f7d2fd0eeb0be8863427a06f26667579
OmarZerir <omarzerir@gmail.com>
2021-11-14 10:02:02 +0300

    swipe to delete

--- RecyclerView/app/src/main/java/com/zerir/recyclerview/BindingAdapterRecyclerview.kt
+++ RecyclerView/app/src/main/java/com/zerir/recyclerview/BindingAdapterRecyclerview.kt
@@ -1,13 +1,48 @@
 package com.zerir.recyclerview
 
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-@BindingAdapter(value = ["adapter", "isFixedSize"])
+@BindingAdapter(value = ["adapter", "isFixedSize", "swipeToDoAction"])
 fun RecyclerView.setupAdapter(
     adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?,
-    isFixedSize: Boolean?
+    isFixedSize: Boolean? = null,
+    swipeToDoAction: SwipeToDoAction? = null,
 ) {
     this.setHasFixedSize(isFixedSize ?: true)
     this.adapter = adapter
+    swipeToDoAction?.let {
+        val itemTouch = generateItemTouch(swipeToDoAction)
+        val itemTouchHelper = ItemTouchHelper(itemTouch)
+        itemTouchHelper.attachToRecyclerView(this)
+    }
+}
+
+interface SwipeToDoAction {
+    fun action(position: Int)
+}
+
+private fun generateItemTouch(swipeToDoAction: SwipeToDoAction): ItemTouchHelper.SimpleCallback {
+    return object : ItemTouchHelper.SimpleCallback(
+        0,
+        ItemTouchHelper.LEFT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            when (direction) {
+                ItemTouchHelper.LEFT -> {
+                    val position = viewHolder.absoluteAdapterPosition
+                    swipeToDoAction.action(position)
+                }
+            }
+        }
+    }
 }
--- RecyclerView/app/src/main/java/com/zerir/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/zerir/recyclerview/MainActivity.kt
@@ -7,10 +7,11 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.zerir.recyclerview.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), OnItemClickListener {
+class MainActivity : AppCompatActivity(), OnItemClickListener, SwipeToDoAction {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var adapter: ListAdapter
     private val list = mutableListOf<ListItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +19,31 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         list.addAll(loadDataManual())
-        binding.adapter = setupAdapter(list, this)
+        adapter = setupAdapter(list, this)
+        binding.adapter = adapter
+        binding.action = this
     }
 
     override fun onItemClicked(item: ListItem, position: Int) {
         val message = "Item #$position Clicked!\nH: ${item.head}, D: ${item.desc}"
         Log.d("Item", message)
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun action(position: Int) {
+        deleteAndUpdateUi(position)
+    }
+
+    private fun deleteAndUpdateUi(position: Int) {
+        //delete if only odd number
+        if(position % 2 != 0) {
+            list.removeAt(position)
+            adapter.notifyItemRemoved(position)
+        }
+        // reverse the swipe
+        else {
+            adapter.notifyItemChanged(position)
+        }
     }
 
     private fun setupAdapter(list: List<ListItem>, onItemClickListener: OnItemClickListener): ListAdapter {
